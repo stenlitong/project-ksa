6824d521d0d82e28b412c622536b9c68bb3b0774
rhnnauf <raihannauf@gmail.com>
2021-09-18 15:46:06 +0700

    fix form

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,7 +13,7 @@ class DashboardController extends Controller
             return view('crew.crewDashboard', compact('orders'));
         }elseif(Auth::user()->hasRole('logistic')){
             // $orders = Order::where('in_progress', 'in_progress(Logistic)')->orderBy('created_at', 'desc')->get();
-            $orders = Order::orderBy('in_progress', 'asc')->Paginate(10);
+            $orders = Order::orderBy('created_at', 'desc')->Paginate(10);
             return view('logistic.logisticDashboard', compact('orders'));
         }elseif(Auth::user()->hasRole('purchasing')){
             return view('purchasing.purchasingDashboard');
--- resources/views/logistic/logisticApproveOrder.blade.php
+++ resources/views/logistic/logisticApproveOrder.blade.php
@@ -8,53 +8,71 @@
 
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
         <div class="mt-5 mb-5">
-            <h2 class="text-center">Create Purchase Requisition</h2>
+            <h2 class="text-center mb-5">Create Purchase Requisition</h2>
             <form>
-                <div class="form-row">
+                <div class="form-row justify-content-between">
                     <div class="form-group col-md-6">
-                        <label for="inputEmail4">Email</label>
-                        <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+                        <label for="inputEmail4">Nama Kapal</label>
+                        <input type="email" class="form-control" id="inputEmail4" placeholder="Nama Kapal">
                     </div>
-                    <div class="form-group col-md-6">
-                        <label for="inputPassword4">Password</label>
-                        <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="inputAddress">Address</label>
-                    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-                </div>
-                <div class="form-group">
-                    <label for="inputAddress2">Address 2</label>
-                    <input type="text" class="form-control" id="inputAddress2"
-                        placeholder="Apartment, studio, or floor">
-                </div>
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <label for="inputCity">City</label>
-                        <input type="text" class="form-control" id="inputCity">
-                    </div>
-                    <div class="form-group col-md-4">
-                        <label for="inputState">State</label>
+                    {{-- <div class="form-group col-md-5">
+                        <label for="inputState">Department</label>
                         <select id="inputState" class="form-control">
-                            <option selected>Choose...</option>
-                            <option>...</option>
+                            <option>Deck</option>
+                            <option>Mesin</option>
+                        </select>
+                    </div> --}}
+                </div>
+                <div class="form-row justify-content-between">
+                    <div class="form-group col-md-3">
+                        <label for="inputEmail4">Department</label>
+                        <input type="text" class="form-control" id="inputEmail4" placeholder="deck" readonly>
+                    </div>
+                    <div class="form-group col-md-3">
+                        <label for="inputCity">Perusahaan</label>
+                        <input type="text" class="form-control" id="inputCity" placeholder="Ex : ISA">
+                    </div>
+                    <div class="form-group col-md-3">
+                        <label for="inputState">Daerah</label>
+                        <select id="inputState" class="form-control">
+                            <option>Jakarta</option>
+                            <option>Samarinda</option>
+                            <option>Banjarmasin</option>
+                            <option>Maluku</option>
+                            <option>Medan</option>
+                            <option>Bunati</option>
                         </select>
                     </div>
-                    <div class="form-group col-md-2">
-                        <label for="inputZip">Zip</label>
-                        <input type="text" class="form-control" id="inputZip">
+                    <div class="form-group col-md-3">
+                        <label for="inputZip">Item Barang</label>
+                        <input type="text" class="form-control" id="inputZip" placeholder="Ex : Tali">
                     </div>
                 </div>
-                <div class="form-group">
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" id="gridCheck">
-                        <label class="form-check-label" for="gridCheck">
-                            Check me out
-                        </label>
+                <div class="form-row justify-content-between">
+                    <div class="form-group col-md-3">
+                        <label for="inputEmail4">Tanggal PR</label>
+                        <input type="date" class="form-control" id="inputEmail4" placeholder="Email">
+                    </div>
+                    <div class="form-group col-md-3">
+                        <label for="inputEmail4">Serial Number / Part Number</label>
+                        <input type="text" class="form-control" id="inputEmail4" placeholder="">
+                    </div>
+                    <div class="form-group col-md-3">
+                        <label for="inputEmail4">Quantity</label>
+                        <input type="text" class="form-control" id="inputEmail4" placeholder="12 PCS" readonly>
+                    </div>
+                    <div class="form-group col-md-3">
+                        <label for="inputEmail4">Code Master Item</label>
+                        <input type="text" class="form-control" id="inputEmail4" placeholder="">
                     </div>
                 </div>
-                <button type="submit" class="btn btn-primary">Sign in</button>
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label for="inputAddress2">Note</label>
+                        <textarea class="form-control" name="reason" id="reason" rows="3"></textarea>
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary mt-3">Submit</button>
             </form>
         </div>
     </main>
