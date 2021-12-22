11c7dcf84a6fc2b536ebf8ad4a658ecd508d9f52
rhnnauf <raihannauf@gmail.com>
2021-12-22 14:45:28 +0700

    fix

--- resources/views/crew/crewCreateTask.blade.php
+++ resources/views/crew/crewCreateTask.blade.php
@@ -9,7 +9,7 @@
             @include('crew.sidebar')
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                 <div class="flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 mt-3">
-                    <h1 class="mt-3 mb-5"  style="text-align: center">Create Task</h1>
+                    <h1 class="mt-3 mb-5" style="text-align: center">Create Task</h1>
                     
                     <div>
                         <form method="POST" action="">
--- resources/views/crew/crewCreateTaskDetail.blade.php
+++ resources/views/crew/crewCreateTaskDetail.blade.php
@@ -10,6 +10,29 @@
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                 <div class="flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 mt-3">
                     
+                    <form action="" method="post">
+                        @csrf
+
+                        <div class="form-row">
+                            <div class="card border-dark mb-3" style="width: 18rem;">
+                                <div class="card-body">
+                                    <h5 class="card-title text-center">F/A Jetty</h5>
+                                    <h6 class="card-subtitle mb-2 text-muted text-center text-center">Start Date & Time</h6>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="card border-dark mb-3" style="width: 18rem;">
+                                <div class="card-body">
+                                    <h5 class="card-title text-center">F/A Jetty</h5>
+                                    <h6 class="card-subtitle mb-2 text-muted text-center text-center">Start Date & Time</h6>
+                                    
+                                </div>
+                            </div>
+                        </div>
+
+                    </form>
 
                 </div>
             </main>
