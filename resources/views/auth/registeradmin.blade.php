07e16491c0ecd78b35510c4eb2cc54e190115844
rhnnauf <raihannauf@gmail.com>
2022-01-18 09:01:11 +0700

    fix register

--- resources/views/auth/registeradmin.blade.php
+++ resources/views/auth/registeradmin.blade.php
@@ -152,22 +152,24 @@
                             document.getElementById("Babelan").disabled = true;
                             document.getElementById("Berau").disabled = true;
                             document.getElementById("Kendari").disabled = true;
-                        }else{
-                            document.getElementById("Samarinda").disabled = false;
-                            document.getElementById("Banjarmasin").disabled = false;
-                            document.getElementById("Bunati").disabled = false;
-                            document.getElementById("Babelan").disabled = false;
-                            document.getElementById("Berau").disabled = false;
-                            document.getElementById("Kendari").disabled = false;
                         }
+                        // else{
+                        //     document.getElementById("Samarinda").disabled = false;
+                        //     document.getElementById("Banjarmasin").disabled = false;
+                        //     document.getElementById("Bunati").disabled = false;
+                        //     document.getElementById("Babelan").disabled = false;
+                        //     document.getElementById("Berau").disabled = false;
+                        //     document.getElementById("Kendari").disabled = false;
+                        // }
                         
                         if (dropdown.value=="picSite") {
                             document.getElementById("Jakarta").disabled = true;
                             selectopt('Samarinda');
                             
-                        } else {
-                            document.getElementById("Jakarta").disabled = false;
-                        }
+                        } 
+                        // else {
+                        //     document.getElementById("Jakarta").disabled = false;
+                        // }
                         if(dropdown.value == "adminPurchasing" || dropdown.value == "purchasingManager"){
                             selectopt('Jakarta');
                             document.getElementById("Banjarmasin").disabled = true;
@@ -176,15 +178,16 @@
                             document.getElementById("Babelan").disabled = true;
                             document.getElementById("Berau").disabled = true;
                             document.getElementById("Kendari").disabled = false;
-                        }else{
-                            document.getElementById("Jakarta").disabled = false;
-                            document.getElementById("Samarinda").disabled = false;
-                            document.getElementById("Banjarmasin").disabled = false;
-                            document.getElementById("Bunati").disabled = false;
-                            document.getElementById("Babelan").disabled = false;
-                            document.getElementById("Berau").disabled = false;
-                            document.getElementById("Kendari").disabled = false;
                         }
+                        // else{
+                        //     document.getElementById("Jakarta").disabled = false;
+                        //     document.getElementById("Samarinda").disabled = false;
+                        //     document.getElementById("Banjarmasin").disabled = false;
+                        //     document.getElementById("Bunati").disabled = false;
+                        //     document.getElementById("Babelan").disabled = false;
+                        //     document.getElementById("Berau").disabled = false;
+                        //     document.getElementById("Kendari").disabled = false;
+                        // }
                     }
                     </script>
         </form>
